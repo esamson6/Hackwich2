03f4f3e7d63a2787b1363f3315e46a5f517ded7a
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-02-03 22:33:32 -1000

    Completed Parts 4-6

---
+++ Hackwich2/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  Hackwich2
+//
+//  Created by Erin Samson on 2/3/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func changeColorButtonPressed(_ sender: Any)
+{
+  //code block
+  //change the background color of the button when the "Change Color" button is pressed
+        self.view.backgroundColor = UIColor.purple
+        
+        
+    
+    }
+    
+    }
+    
+
+
