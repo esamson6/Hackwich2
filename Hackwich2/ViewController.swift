568966bc260ee09c9b4a677607bb376da7076690
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-02-03 21:13:56 -1000

    Completed Part 8

--- Hackwich2/ViewController.swift
+++ Hackwich2/ViewController.swift
@@ -26,9 +26,17 @@ class ViewController: UIViewController {
     
     //Part 7 Number 5
         print("Hello World!")
+    }
     
+    //Part 7 - I am going to create my second IB Action
     
-    
+    @IBAction func pressMeButtonPressed2(_ sender: Any) {
+        
+        //This is the code block for the pressMeButtonPressed2 function
+        
+        //Part 8 Number 6
+        print("I got this")
+        
     }
     
     
